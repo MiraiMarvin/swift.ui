ef08e2f98bb9a787ad7d6be6592c2587dcf3aeb7
MiraiMarvin <marvin.clerc@edu.devinci.fr>
2024-10-15 14:00:18 +0200

    Initial Commit

---
+++ swift.ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift.ui
+//
+//  Created by CLERC Marvin on 15/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! mec")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift.ui/swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_uiApp.swift
+//  swift.ui
+//
+//  Created by CLERC Marvin on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
